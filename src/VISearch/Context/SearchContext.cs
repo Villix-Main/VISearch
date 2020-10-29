701df9f2beadd36bda1ec4438d0aec9aa1bffe48
Dominic <Martinez>
2020-10-29 16:14:54 -0400

    Added basic functionality and the constructor to the SearchContext class

--- src/VISearch/Context/SearchContext.cs
+++ src/VISearch/Context/SearchContext.cs
@@ -1,9 +1,13 @@
-﻿namespace VISearch
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace VISearch
 {
     /// <summary>
     /// 
     /// </summary>
     public class SearchContext<TSearchType>
+        where TSearchType : class
     {
         #region Private Members
 
@@ -11,6 +15,17 @@
 
         #region Constructor
 
+        public SearchContext()
+        {
+            // Get the type of TSearchType
+            var serachType = typeof(TSearchType);
+        }
+
         #endregion
+
+        public void BeginPipeline(IEnumerable<TSearchType> collection)
+        {
+
+        }
     }
 }

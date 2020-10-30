de31c450483ef9bc227dbe390a279ce2d6073b6a
Dominic <Martinez>
2020-10-29 23:55:11 -0400

    Added the main functionality of the VISearchAttribute base class

--- src/VISearch/Attributes/Base/VISearchAttribute.cs
+++ src/VISearch/Attributes/Base/VISearchAttribute.cs
@@ -1,8 +1,38 @@
 ﻿using System;
+using System.Xml.Serialization;
 
 namespace VISearch
 {
+    /// <summary>
+    /// The base that contains all basic functionality for all 
+    /// VISearch attributes to derive from. Decorating a entity <br />
+    /// with attribute will serve no purpose as the <see cref="VISearchAttribute"/>
+    /// just serves as a base class.
+    /// </summary>
     public class VISearchAttribute : Attribute
     {
+        #region Public Properties
+        
+        /// <summary>
+        /// The name that will be used by the Search Pipeline. 
+        /// This name will most likely be the name of the class <br />
+        /// member itself.
+        /// </summary>
+        public string Name { get; set; }
+
+        #endregion
+
+        #region Constructor
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VISearchAttribute"/> base class
+        /// </summary>
+        /// <param name="name">
+        /// The name of a class member that will be used by the
+        /// Search Pipeline. This name will most likely be the <br />
+        /// class member itself</param>
+        public VISearchAttribute(string name) => Name = name;
+        
+        #endregion
     }
 }

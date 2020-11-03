fd20d820ed02783e4b6b1c7861fd057277336940
Dominic <Martinez>
2020-11-03 00:06:54 -0500

    Added beta documentation to SearchContext  Class
    The documentation is subject to change over time.

--- src/VISearch/Context/SearchContext.cs
+++ src/VISearch/Context/SearchContext.cs
@@ -1,12 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
+/// <summary>
+/// The main context is the day
+/// </summary>
 namespace VISearch
 {
     /// <summary>
-    /// 
+    /// The context to be used for all VISearch operations such as 
+    /// beginning the Search Pipeline. The context will usually <br />
+    /// be derived from another class and will contain Filters. The 
+    /// search context will also contain validators to validate type <br />
+    /// to meet the Search Pipeline requirements.
     /// </summary>
-    public class SearchContext<TSearchType>
+    /// <typeparam name="TSearchType">
+    /// The type of the collection to be searched and the type to be used
+    /// by the Search Pipeline for comparison with the Search Query. If <br />
+    /// you use Entity Framework, <see cref="TSearchType"/> will most
+    /// likely be the same type of a DbSet. <see cref="TSearchType"/> will
+    /// be required have at least <br /> property or field decorated with the 
+    /// <see cref="SearchItemAttribute"/>. This type will be validated in
+    /// context constructor and any errors will be <br /> given if requirements
+    /// are not met.
+    /// </typeparam>
+    public class SearchContext<TSearchType> 
         where TSearchType : class
     {
         #region Private Members
@@ -15,17 +31,16 @@ namespace VISearch
 
         #region Constructor
 
+        /// <summary>
+        /// The default constructor of the Search Context. The main
+        /// purpose of the constructor is to validate <see cref="TSearchType"/>
+        /// and to make sure it meet's requirements of the Search Pipeline. Any 
+        /// signs of breaking these requirements 
+        /// </summary>
         public SearchContext()
         {
-            // Get the type of TSearchType
-            var serachType = typeof(TSearchType);
         }
 
-        #endregion
-
-        public void BeginPipeline(IEnumerable<TSearchType> collection)
-        {
-
-        }
+        #endregion  
     }
 }

76db152062761d03e034d9c11b0b374c142db8a0
Dominic <Martinez>
2020-11-11 14:18:00 -0500

    Added main enum values and documentation to the SearchType  enum

--- src/VISearch/Search/Settings/SearchType.cs
+++ src/VISearch/Search/Settings/SearchType.cs
@@ -1,6 +1,30 @@
 ﻿namespace VISearch
 {
+    /// <summary>
+    /// The type of search that the Search Pipeline will
+    /// execute. The search type will work with any Search <br />
+    /// Format that does not already have it's own search 
+    /// type such as the <see cref="SearchFormat.FilterString"/> 
+    /// </summary>
     public enum SearchType
     {
+        /// <summary>
+        /// A sectional search will tell the Search Pipeline
+        /// that a search string does not have to have full <br />
+        /// equality with a item in the Searched Collection 
+        /// for it to be added to the Search Results. The <br />
+        /// strictness of a sectional search can be set
+        /// in the <see cref="SearchSettings"/>.
+        /// </summary>
+        Sectional = 0,
+
+        /// <summary>
+        /// A strict search will tell the Search Pipeline
+        /// to make sure that the search string matches <br />
+        /// exact with a item in the Searched collection.
+        /// Strict search settings such as case sensitive <br />
+        /// search can be set in the <see cref="SearchSettings"/>.
+        /// </summary>
+        Strict = 1
     }
 }

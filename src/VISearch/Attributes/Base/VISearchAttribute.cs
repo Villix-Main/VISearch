ee2e3f339594480cb2f7d09c084ada1c4d2b264c
Dominic <Martinez>
2021-01-05 22:15:30 -0500

    Removed the Name property from class
    Removed the property as the property itself did not make sense for search pipeline

--- src/VISearch/Attributes/Base/VISearchAttribute.cs
+++ src/VISearch/Attributes/Base/VISearchAttribute.cs
@@ -10,27 +10,12 @@ namespace VISearch
     /// </summary>
     public class VISearchAttribute : Attribute
     {
-        #region Public Properties
-        
-        /// <summary>
-        /// The name that will be used by the Search Pipeline. 
-        /// This name will most likely be the name of the class <br />
-        /// member itself.
-        /// </summary>
-        public string Name { get; set; }
-
-        #endregion
-
         #region Constructor
-        
+
         /// <summary>
         /// Initializes a new instance of the <see cref="VISearchAttribute"/> base class
         /// </summary>
-        /// <param name="name">
-        /// The name of a class member that will be used by the
-        /// Search Pipeline. This name will most likely be the <br />
-        /// class member itself</param>
-        public VISearchAttribute(string name) => Name = name;
+        public VISearchAttribute() { }
         
         #endregion
     }
